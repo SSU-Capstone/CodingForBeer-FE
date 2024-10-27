6e7b417537ee78e986ca68d0d257b6cd473b54a6
m4ushold <m4ushold@gmail.com>
2024-10-27 20:15:59 +0900

    remove: kakao social login

--- src/components/LoginoutButton.jsx
+++ src/components/LoginoutButton.jsx
@@ -3,78 +3,76 @@ import { useRecoilState } from "recoil";
 import { loginState } from "../recoilState";
 import { useState } from "react";
 import SocialGoogle from "./SocialGoogle";
-import SocialKakao from "./SocialKakao";
 import { useNavigate } from "react-router-dom";
 
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    boxShadow: 24,
-    borderRadius: '2rem',
-    p: 4,
-    display: 'flex',
-    flexDirection: 'column',
-    maxHeight: '300px',
-    height: '100%',
-    justifyContent: 'space-around',
-  }; 
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  borderRadius: '2rem',
+  p: 4,
+  display: 'flex',
+  flexDirection: 'column',
+  maxHeight: '300px',
+  height: '100%',
+  justifyContent: 'space-around',
+}; 
 
 const LoginoutButton = () => {
-    const [isLogin, setIsLogin] = useRecoilState(loginState);
+  const [isLogin, setIsLogin] = useRecoilState(loginState);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    setIsLogin(false);
+    navigate('/');
+  }
   
-    const handleClose = () => {
-      setOpen(false);
-    };
-
-    const navigate = useNavigate();
-    const handleLogout = () => {
-      setIsLogin(false);
-      navigate('/');
-    }
-    
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-    return (
+const [open, setOpen] = useState(false);
+const handleOpen = () => setOpen(true);
+  return (
+    <>
+    {isLogin ? 
+        <Button color="inherit" onClick={handleLogout}>Logout</Button>
+        :
         <>
-        {isLogin ? 
-            <Button color="inherit" onClick={handleLogout}>Logout</Button>
-            :
-            <>
-                <Button color="inherit" onClick={handleOpen}>Login</Button>
-                <Modal
-                    aria-labelledby="transition-modal-title"
-                    aria-describedby="transition-modal-description"
-                    open={open}
-                    onClose={handleClose}
-                    closeAfterTransition
-                    slots={{ backdrop: Backdrop }}
-                    slotProps={{
-                    backdrop: {
-                        timeout: 500,
-                    },
-                    }}
-                >
-                    <Fade in={open}>
-                    <Box sx={style}>
-                        <Typography id="transition-modal-title" variant="h6" component="h2">
-                          로그인
-                        </Typography>
+          <Button color="inherit" onClick={handleOpen}>Login</Button>
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            slots={{ backdrop: Backdrop }}
+            slotProps={{
+            backdrop: {
+                timeout: 500,
+            },
+            }}
+          >
+            <Fade in={open}>
+            <Box sx={style}>
+              <Typography id="transition-modal-title" variant="h6" component="h2">
+                로그인
+              </Typography>
 
-                        <div className={'flex flex-col gap-5'}>
-                          <SocialGoogle />
-                          <SocialKakao />
-                        </div>
-                    </Box>
-                    </Fade>
-                </Modal>
-            </>
-        }
-        </>
-    )
+              <div className={'flex flex-col gap-5'}>
+                <SocialGoogle />
+              </div>
+            </Box>
+            </Fade>
+          </Modal>
+      </>
+    }
+    </>
+  )
 
 }
 
--- src/components/SocialGoogle.jsx
+++ src/components/SocialGoogle.jsx
@@ -3,34 +3,24 @@ import {GoogleOAuthProvider} from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 
 const SocialGoogle = () => {
-    const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-    const navigate = useNavigate();
-    const LoginSuccess = async (data)=>{
-        // Todo : 엑세스 토큰 백엔드로 전달
-        console.log(data)
-        
-        
-        navigate('/dashboard');
-    }
-    const LoginFail = (error) => {
-        console.log(error);
-    };
+  const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+  const navigate = useNavigate();
+  const LoginSuccess = ()=>{
+    navigate('/dashboard');
+  }
+  const LoginFail = (error) => {
+    console.log(error);
+  };
 
-    return (
-        <>
-            <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-                <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-
-                        <GoogleLogin
-                          onSuccess={LoginSuccess}
-                          onFailure={LoginFail}
-                          width={336}
-                        />
-
-                </GoogleOAuthProvider>
-            </GoogleOAuthProvider>
-        </>
-    );
+  return (
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <GoogleLogin
+        onSuccess={LoginSuccess}
+        onFailure={LoginFail}
+        width={336}
+      />
+    </GoogleOAuthProvider>
+  );
 };
 
 export default SocialGoogle
