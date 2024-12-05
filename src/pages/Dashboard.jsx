49851a31eb43415283a55fe2b14a71c4e6771595
Jung Joo Hyun <inthemman@gmail.com>
2024-12-05 15:02:26 +0900

    yorkie googdt

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -203,7 +203,6 @@ export default function Dashboard() {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ document_name: documentName }),
       credentials: 'include',
     });
     const data = await response.json();
